8a9c5f28746a588e70bc9be600873e75f36268ba
romanbond <romanjbond@gmail.com>
2016-08-08 14:49:28 -0400

    Added Good Ticker

---
+++ stock-dashboard(good)/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/alexerlandsson/pen/YXXzLR.
+
+ Stock dashboard that uses Yahoo Finance API to get stock data.
---
+++ stock-dashboard(good)/js/index.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+	var stocks = ['NVDA', 'EBAY', 'INTC', 'AAPL', 'MSFT', 'GOOG', 'FB', 'TWTR'];
+	var stocksUrl = stocks.join('%20');
+
+	var url = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%3D%22' + stocksUrl + '%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=';
+
+	$.getJSON(url, function(data) {
+		for (var i = 0; i < data.query.results.quote.length; i++) {
+			var stockId = '#stock-' + (i + 1);
+			var change = data.query.results.quote[i].ChangeinPercent;
+
+			if (change.slice(0,-1) < 0) {
+				$(stockId).css('background-color', '#db5959');
+			}  else if (change.slice(0,-1) > 0) {
+				$(stockId).css('background-color', '#68b665');
+			} else if (change.slice(0,-1) == 0) {
+				$(stockId).css('background-color', '#fdca41');
+			}
+
+			$(stockId).children('.symbol').html(data.query.results.quote[i].symbol);
+			$(stockId).children('.change').html(change);
+		}
+	});
+
+});
